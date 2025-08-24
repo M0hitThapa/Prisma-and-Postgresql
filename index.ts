8051058316c98cb2f8a37a967a950ccf9ef82a1a
Mohit Thapa <thapamohit2058@gmail.com>
2025-08-25 00:01:04 +0545

    using prisma different attributes in the routes

--- index.ts
+++ index.ts
@@ -8,12 +8,36 @@ app.use(express.json())
 app.get("/users", async(_, res) => {
 const users = await prisma.user.findMany({
     where:{
-      nationality: {
-        not:"Indian"
-      }
+     nationality:{
+        in:["Indian", "Chinese", "French"]
+     }
     }
 })
 res.json(users)
+})
+
+app.put("/users", async (_,res) => {
+    const updateUser = await prisma.user.update({
+        where:{
+            email:"alice.johnson@example.com"
+        },
+        data:{
+            age:12,
+            isMarried:true
+        }
+    })
+    res.json(updateUser)
+
+})
+
+
+app.delete("/users", async (_,res) => {
+    const deleteUser = await prisma.user.deleteMany({
+        where:{
+            age:{gt:30}
+        }
+    })
+    res.json(deleteUser)
 })
 
 app.listen(3000, () => {
